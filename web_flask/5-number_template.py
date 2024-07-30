9e0f5bb43dff4c5d204de6e9113e8c82a23afc5a
hima890 <hfibrahim90@gmail.com>
2024-07-30 04:50:37 +0200

    bugFix/all-5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,80 +1,54 @@
 #!/usr/bin/python3
-"""
-This module starts a Flask web application with several routes:
-- '/' which returns 'Hello HBNB!'
-- '/hbnb' which returns 'HBNB'
-- '/c/<text>' which returns 'C ' followed by the value of the text variable,
-with underscores replaced by spaces.
-- '/python/(<text>)' which returns 'Python ', followed by the value of the
-text variable or the default 'is cool', with underscores replaced by spaces.
-- '/number/<int:n>' which dynamically returns an HTML page displaying
-'Number: n', where n is an integer.
-"""
+""" 5. Add fifth view func that displays HTML page if n is int """
 
 from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Returns a greeting message 'Hello HBNB!' when accessing the root path.
-    """
-    return ('Hello HBNB!')
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def show_HBNB():
-    """
-    Returns 'HBNB' when accessing the '/hbnb' path.
-    """
-    return ('HBNB')
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def return_text(text):
-    """
-    Returns 'C ' followed by the value of the text variable,
-    with underscores replaced by spaces,when accessing the
-    '/c/<text>' path.
-    """
-    text = text.replace('_', ' ')  # Replace underscores with spaces
-    return "C {}".format(text)
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def return_python_text(text):
-    """
-    Returns 'Python ' followed by the value of the text variable
-    or the default 'is cool', with underscores replaced by spaces,
-    when accessing the '/python/' or '/python/<text>' path.
-    """
-    text = text.replace('_', ' ')  # Replace underscores with spaces
-    return "Python {}".format(text)
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """
-    Returns an HTML page displaying 'Number: n', where n is an integer,
-    when accessing the '/number/<n>' path.
-    """
-    return (
-        """<!DOCTYPE html>
-    <HTML lang="en">
-        <HEAD>
-            <TITLE>HBNB</TITLE>
-        </HEAD>
-        <BODY>
-            <H1>Number: {}</H1>
-        </BODY>
-    </HTML>""".format(n)
-            )
+@app.route('/number/<int:n>')
+def number_text(n):
+    """ replace with int only if given int. """
+    n = str(n)
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def html_num(n):
+    """ display html if n is int. """
+    n = str(n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=5000,
-            )
+    app.run(host='0.0.0.0', port=5000)
